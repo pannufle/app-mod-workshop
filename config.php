edc3519821ad210f83b7b093b14a8d979585bd35
Jeremy Hottinger <jeremy.hottinger@Mac-JDGJ7TFX0H.local>
2025-06-19 15:31:36 +0200

    trigger build

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ $db_host = getenv('DB_HOST') ?: 'localhost';
 $db_name = getenv('DB_NAME') ?: 'image_catalog';
 $db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
 $db_pass = getenv('DB_PASS') ?: 'wrong_password';
-
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
